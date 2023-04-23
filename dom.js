a54b932e5966da56940ca84e16aa342c1a395f69
joseiberico <josesito.x_x@hotmail.com>
2023-04-23 17:18:20 -0500

    AppJS

---
+++ dom.js
@@ -0,0 +1,29 @@
+const contenedor = document.querySelector(".flex-container");
+
+function crearLlave(nombre,modelo,precio){
+   img = `<img class="llave-img" src="llave.png">`;
+   nombre = `<h2><b>${nombre}</b></h2>`
+   modelo = `<h3 class="${modelo}"><b>${modelo}</b></h3>`;
+   precio = `<p>Precio: <b>$${precio}</b></p>`;
+   return [img,nombre,modelo,precio];
+}
+
+const changeHidden = (number) =>{
+   document.querySelector(".key-data").value = number;
+}
+
+let documentFragment = document.createDocumentFragment();
+
+for(var i = 1; i <= 20; i++){
+   let modeloRandom = Math.round(Math.random() * 10000);
+   let precioRandom = Math.round(Math.random() * 10 + 30);
+   let llave = crearLlave(`Llave: ${i}`,`Modelo: ${modeloRandom}`,precioRandom);
+   let div = document.createElement("div");
+   div.addEventListener("click",()=>{changeHidden(modeloRandom)});
+   div.tabIndex = i;
+   div.classList.add(`item-${i}`,`flex-item`);
+   div.innerHTML = llave[0] + llave[1] + llave[2] + llave[3];
+   documentFragment.appendChild(div);
+}
+
+contenedor.appendChild(documentFragment);
